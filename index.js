d73c182385b4803ab428fcac1a06399ad732a352
Abdus Samad <abdussamadshamsi486@gmail.com>
2025-04-15 20:24:43 +0530

    minor change and .env

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
+require("dotenv").config();
 const express = require("express");
-const path = require("path")
-const mongoose = require("mongoose")
-const cookieParser = require("cookie-parser")
+const path = require("path");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
-const Blog = require("./models/blog.model")
+const Blog = require("./models/blog.model");
 
 const userRoute = require("./routes/user.route");
-const blogRoute = require("./routes/blog.routes")
+const blogRoute = require("./routes/blog.routes");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connect("mongodb://127.0.0.1:27017/node-narrate")
+mongoose.connect(MONGO_URL)
 .then(() => console.log("MongoDB Connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.json());
-app.use(checkForAuthenticationCookie("token"))
-app.use(express.static(path.resolve("./public")))
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
-app.set('view engine', 'ejs')
-app.set("views", path.resolve("./views"))
+app.set('view engine', 'ejs');
+app.set("views", path.resolve("./views"));
 
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     const allBlogs = await Blog.find({}).sort({ createdAt: -1 });
-    //console.log(allBlogs);
-    
     res.render("home", {
         user: req.user,
         blogs: allBlogs
     });
 });
 
+app.use('/user', userRoute);
+app.use('/blog', blogRoute);
 
-app.use('/user', userRoute)
-app.use('/blog', blogRoute)
-
-app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`))
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
